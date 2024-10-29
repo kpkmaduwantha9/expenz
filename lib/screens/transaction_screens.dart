57d4b8007436b7c754c2557a76245022ca41436f
kpkmaduwantha <kpkmaduwantha99@gmail.com>
2024-10-29 08:17:51 +0530

    A Revert "show and dismiss expense"
    This reverts commit be94526bd6c5d66e9d32f225079a36fbb5964968.
    
    :wq
    :q

--- lib/screens/transaction_screens.dart
+++ lib/screens/transaction_screens.dart
@@ -1,19 +1,7 @@
-import 'package:expenz/constants/colors.dart';
-import 'package:expenz/constants/constants.dart';
-import 'package:expenz/widgets/expense_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-import '../models/expense_model.dart';
 
 class TransactionScreen extends StatefulWidget {
-  final List<Expense> expensesList;
-  final void Function(Expense) onDismissedExpense;
-  const TransactionScreen({
-    super.key,
-    required this.expensesList,
-    required this.onDismissedExpense,
-  });
+  const TransactionScreen({super.key});
 
   @override
   State<TransactionScreen> createState() => _TransactionScreenState();
@@ -23,73 +11,8 @@ class _TransactionScreenState extends State<TransactionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(kDefaultPadding),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "See your financial report",
-                style: TextStyle(
-                  fontSize: 20.sp,
-                  fontWeight: FontWeight.w500,
-                  color: kMainColor,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Expenses",
-                style: TextStyle(
-                  fontSize: 20.sp,
-                  fontWeight: FontWeight.bold,
-                  color: kBlack,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.35,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      ListView.builder(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.vertical,
-                        physics: NeverScrollableScrollPhysics(),
-                        itemCount: widget.expensesList.length,
-                        itemBuilder: (context, index) {
-                          final expense = widget.expensesList[index];
-
-                          return Dismissible(
-                            key: ValueKey(expense),
-                            direction: DismissDirection.startToEnd,
-                            onDismissed: (direction) {
-                              setState(() {
-                                widget.onDismissedExpense(expense);
-                              });
-                            },
-                            child: ExpenseCard(
-                              title: expense.title,
-                              date: expense.date,
-                              amount: expense.amount,
-                              category: expense.category,
-                              description: expense.description,
-                              time: expense.time,
-                            ),
-                          );
-                        },
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        title: Text("dd"),
       ),
     );
   }
